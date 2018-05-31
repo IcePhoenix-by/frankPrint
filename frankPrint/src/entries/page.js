748566ef131f21233dd794a3816adf21d534ec2f
Евгений <specterby@gmail.com>
2018-05-31 10:56:37 +0300

    init

---
+++ frankPrint/src/entries/page.js
@@ -0,0 +1,11 @@
+﻿import Vue from 'vue';
+import Vuetify from 'vuetify'
+Vue.use(Vuetify)
+import main from "../components/main.vue";
+    new Vue({
+        el: '#root',
+        render: h => h(main)
+    });
+
+
+
---
+++ frankPrint/webpack.config.js
@@ -0,0 +1,60 @@
+﻿var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+    entry: './src/entries/page.js',
+    output: {
+      path: path.join(__dirname, 'wwwroot/dist'),
+      filename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    loaders: {
+                    }
+                }
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]?[hash]'
+                }
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js'
+        }
+    },
+    devtool: '#eval-source-map'
+}
+
+if (process.env.NODE_ENV === 'production') {
+    module.exports.devtool = '#source-map'
+    module.exports.plugins = (module.exports.plugins || []).concat([
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"'
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            sourceMap: true,
+            compress: {
+                warnings: false
+            }
+        }),
+        new webpack.LoaderOptionsPlugin({
+            minimize: true
+        })
+    ])
+}
