6098e130c49ba43cdb984bfdea2797cb96239eb5
Евгений <specterby@gmail.com>
2018-06-03 21:23:51 +0300

    qwewq

--- frankPrint/src/components/main.vue
+++ frankPrint/src/components/main.vue
@@ -3,32 +3,132 @@
         <v-toolbar dark color="dark" style="position:absolute;top:0;opacity:0.7;z-index:1000; ">
 
             <v-toolbar-title id="logoText">frank</v-toolbar-title>
+            <!--<div id="logoButtons">
+                <v-btn icon>
+                    <v-icon>search</v-icon>
+                </v-btn>
+                <v-btn icon>
+                    <v-icon>apps</v-icon>
+                </v-btn>
+                <v-btn icon>
+                    <v-icon>refresh</v-icon>
+                </v-btn>
+                <v-btn icon>
+                    <v-icon>more_vert</v-icon>
+                </v-btn>
+            </div>-->
+            <div id="logoContacts">
+                <div style="display:flex;">
+                    <svg height="45px" width="45px">
+                        <use xlink:href="#vk" />
+                    </svg>
+                    <svg version="1.1" id="Layer_1" height="40px" width="40px" x="0px" y="0px"
+                         viewBox="0 0 551.034 551.034" style="enable-background:new 0 0 551.034 551.034;" xml:space="preserve">
 
-            <v-btn icon>
-                <v-icon>search</v-icon>
-            </v-btn>
-            <v-btn icon>
-                <v-icon>apps</v-icon>
-            </v-btn>
-            <v-btn icon>
-                <v-icon>refresh</v-icon>
-            </v-btn>
-            <v-btn icon>
-                <v-icon>more_vert</v-icon>
-            </v-btn>
+	
+                    <linearGradient id="SVGID_1_" gradientUnits="userSpaceOnUse" x1="275.517" y1="4.57" x2="275.517" y2="549.72" gradientTransform="matrix(1 0 0 -1 0 554)">
+                    <stop offset="0" style="stop-color:#E09B3D" />
+
+
+
+                    <stop offset="0.3" style="stop-color:#C74C4D" />
+
+
+
+                    <stop offset="0.6" style="stop-color:#C21975" />
+
+
+
+                    <stop offset="1" style="stop-color:#7024C4" />
+
+
+
+	</linearGradient>
+                    <path style="fill:url(#SVGID_1_);" d="M386.878,0H164.156C73.64,0,0,73.64,0,164.156v222.722
+		c0,90.516,73.64,164.156,164.156,164.156h222.722c90.516,0,164.156-73.64,164.156-164.156V164.156
+		C551.033,73.64,477.393,0,386.878,0z M495.6,386.878c0,60.045-48.677,108.722-108.722,108.722H164.156
+		c-60.045,0-108.722-48.677-108.722-108.722V164.156c0-60.046,48.677-108.722,108.722-108.722h222.722
+		c60.045,0,108.722,48.676,108.722,108.722L495.6,386.878L495.6,386.878z" />
+
+
+
+
+
+
+	
+                    <linearGradient id="SVGID_2_" gradientUnits="userSpaceOnUse" x1="275.517" y1="4.57" x2="275.517" y2="549.72" gradientTransform="matrix(1 0 0 -1 0 554)">
+                    <stop offset="0" style="stop-color:#E09B3D" />
+
+
+
+                    <stop offset="0.3" style="stop-color:#C74C4D" />
+
+
+
+                    <stop offset="0.6" style="stop-color:#C21975" />
+
+
+
+                    <stop offset="1" style="stop-color:#7024C4" />
+
+
+
+	</linearGradient>
+                    <path style="fill:url(#SVGID_2_);" d="M275.517,133C196.933,133,133,196.933,133,275.516s63.933,142.517,142.517,142.517
+		S418.034,354.1,418.034,275.516S354.101,133,275.517,133z M275.517,362.6c-48.095,0-87.083-38.988-87.083-87.083
+		s38.989-87.083,87.083-87.083c48.095,0,87.083,38.988,87.083,87.083C362.6,323.611,323.611,362.6,275.517,362.6z" />
+
+
+
+
+
+
+	
+                    <linearGradient id="SVGID_3_" gradientUnits="userSpaceOnUse" x1="418.31" y1="4.57" x2="418.31" y2="549.72" gradientTransform="matrix(1 0 0 -1 0 554)">
+                    <stop offset="0" style="stop-color:#E09B3D" />
+
+
+
+                    <stop offset="0.3" style="stop-color:#C74C4D" />
+
+
+
+                    <stop offset="0.6" style="stop-color:#C21975" />
+
+
+
+                    <stop offset="1" style="stop-color:#7024C4" />
+
+
+
+	</linearGradient>
+                    <circle style="fill:url(#SVGID_3_);" cx="418.31" cy="134.07" r="34.15" />
+
+</svg>
+
+                    <div>
+                        <div>+375 (29) 6-038-038</div>
+
+                        <div>frank.print@mail.ru</div>
+                    </div>
+
+
+                </div>
+            </div>
+            
         </v-toolbar>
 
 
         <v-content>
             <section>
-                <v-parallax src="assets/hero.jpeg" height="600">
+                <v-parallax src="assets/hero.jpg" height="600" style="">
                     <v-layout column
                               align-center
                               justify-center
                               class="white--text">
-                        <img src="assets/vuetify.png" alt="Vuetify.js" height="200">
-                        <h1 class="white--text mb-2 display-1 text-xs-center">Parallax Template</h1>
-                        <div class="subheading mb-3 text-xs-center">Powered by Vuetify</div>
+
+
+
                         <v-btn class="blue lighten-2 mt-5"
                                dark
                                large
@@ -42,66 +142,66 @@
             <section>
                 <v-layout column
                           wrap
-                          class="my-5"
+                          class="my-0"
                           align-center>
-                    <v-flex xs12 sm4 class="my-3">
-                        <div class="text-xs-center">
-                            <h2 class="headline">The best way to start developing</h2>
-                            <span class="subheading">
-                                Cras facilisis mi vitae nunc
-                            </span>
+
+                    <v-flex xs6>
+                        <div class="advantages-container clearfix">
+                            <div class="advantage">
+                                <div class="advantage-content">
+                                    <div class="row">
+                                        <div class="large-2 medium-2 columns">
+                                            <div class="advantage-content-img advantage-content-img-1"></div>
+                                        </div>
+                                        <div class="large-10 medium-10 columns">
+                                            <h6>ОПЫТ</h6>
+                                            Компания «Цифровая печать» 15 лет на рынке печати, мы гордимся накопленным опытом, выработанным комплексным подходом к решению задач и высоким профессионализмом сотрудников!
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="advantage">
+                                <div class="advantage-content">
+                                    <div class="row">
+                                        <div class="large-2 medium-2 columns">
+                                            <div class="advantage-content-img advantage-content-img-2">
+                                            </div>
+                                        </div>  <div class="large-10 medium-10 columns">
+                                            <h6>ПОЛНЫЙ СПЕКТР УСЛУГ</h6>
+                                            Производственные мощности укомплектованы современным оборудованием для  допечатной подготовки, процесса печати и послепечатной полиграфической обработки. Для разработки макетов или корпоративного стиля печатной продукции для вашей компании мы также предлагаем услуги профессиональных  дизайнеров.
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="advantage">
+                                <div class="advantage-content">
+                                    <div class="row">
+                                        <div class="large-2 medium-2 columns">
+                                            <div class="advantage-content-img advantage-content-img-3">
+                                            </div>
+                                        </div>
+                                        <div class="large-10 medium-10 columns">
+                                            <h6>МЫ - ПРОИЗВОДИТЕЛИ</h6>
+                                            Большинство предложений на рынке полиграфии – посредники, мы же имеем собственный полностью укомплектованный парк печатного оборудования для полного производственного цикла печати любым способом.
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="advantage">
+                                <div class="advantage-content">
+                                    <div class="row">
+                                        <div class="large-2 medium-2 columns">
+                                            <div class="advantage-content-img advantage-content-img-4">
+                                            </div>
+                                        </div>
+                                        <div class="large-10 medium-10 columns">
+                                            <h6>ОПЕРАТИВНОСТЬ</h6>
+                                            Мы идем в ногу с современным темпом развития  бизнеса и готовы выполнить заказ в кратчайшие сроки. Мы гарантируем оперативное исполнение заказа при выполнении оговариваемых требований к печати.
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                    </v-flex>
-                    <v-flex xs12>
-                        <v-container grid-list-xl>
-                            <v-layout row wrap align-center>
-                                <v-flex xs12 md4>
-                                    <v-card class="elevation-0 transparent">
-                                        <v-card-text class="text-xs-center">
-                                            <v-icon x-large class="blue--text text--lighten-2">color_lens</v-icon>
-                                        </v-card-text>
-                                        <v-card-title primary-title class="layout justify-center">
-                                            <div class="headline text-xs-center">Material Design</div>
-                                        </v-card-title>
-                                        <v-card-text>
-                                            Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
-                                            Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-                                            Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti.
-                                        </v-card-text>
-                                    </v-card>
-                                </v-flex>
-                                <v-flex xs12 md4>
-                                    <v-card class="elevation-0 transparent">
-                                        <v-card-text class="text-xs-center">
-                                            <v-icon x-large class="blue--text text--lighten-2">flash_on</v-icon>
-                                        </v-card-text>
-                                        <v-card-title primary-title class="layout justify-center">
-                                            <div class="headline">Fast development</div>
-                                        </v-card-title>
-                                        <v-card-text>
-                                            Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
-                                            Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-                                            Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti.
-                                        </v-card-text>
-                                    </v-card>
-                                </v-flex>
-                                <v-flex xs12 md4>
-                                    <v-card class="elevation-0 transparent">
-                                        <v-card-text class="text-xs-center">
-                                            <v-icon x-large class="blue--text text--lighten-2">build</v-icon>
-                                        </v-card-text>
-                                        <v-card-title primary-title class="layout justify-center">
-                                            <div class="headline text-xs-center">Completely Open Sourced</div>
-                                        </v-card-title>
-                                        <v-card-text>
-                                            Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
-                                            Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-                                            Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti.
-                                        </v-card-text>
-                                    </v-card>
-                                </v-flex>
-                            </v-layout>
-                        </v-container>
                     </v-flex>
                 </v-layout>
             </section>
@@ -176,10 +276,68 @@
                 </v-container>
             </section>
 
-            <v-footer class="blue darken-2">
-                <v-layout row wrap align-center>
 
-                </v-layout>
+
+
+
+                <v-speed-dial v-model="fab"
+                              :top="top"
+                              :bottom="bottom"
+                              :right="right"
+                              :left="left"
+                              :direction="direction"
+                              :open-on-hover="hover"
+                              :transition="transition">
+                    <v-btn slot="activator"
+                           v-model="fab"
+                           color="blue darken-2"
+                           dark
+                           fab>
+                        <v-icon>account_circle</v-icon>
+                        <v-icon>close</v-icon>
+                    </v-btn>
+                    <v-btn fab
+                           dark
+                           small
+                           color="green">
+                        <v-icon>edit</v-icon>
+                    </v-btn>
+                    <v-btn fab
+                           dark
+                           small
+                           color="indigo">
+                        <v-icon>add</v-icon>
+                    </v-btn>
+                    <v-btn fab
+                           dark
+                           small
+                           color="red">
+                        <v-icon>delete</v-icon>
+                    </v-btn>
+                </v-speed-dial>
+
+
+
+
+            <v-footer height="auto">
+                <v-card flat
+                        tile
+                        class="indigo lighten-1 white--text text-xs-center">
+                    <v-card-text>
+                        <v-btn v-for="icon in icons"
+                               :key="icon"
+                               icon
+                               class="mx-3 white--text">
+                            <v-icon size="24px">{{ icon }}</v-icon>
+                        </v-btn>
+                    </v-card-text>
+                    <v-card-text class="white--text pt-0">
+                        Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+                    </v-card-text>
+                    <v-card-text class="white--text">
+                        &copy;2018 — <strong>Vuetify</strong>
+                    </v-card-text>
+                </v-card>
             </v-footer>
         </v-content>
     </v-app>
@@ -187,11 +345,45 @@
 
 <script>
     export default {
-        data: function () {
-            return {
-                title: 'frank'
+        data: () => ({
+            direction: 'top',
+            fab: false,
+            fling: false,
+            hover: false,
+            tabs: null,
+            top: false,
+            right: true,
+            bottom: true,
+            left: false,
+            transition: 'slide-y-reverse-transition',
+            icons: ['fab fa-facebook', 'fab fa-twitter', 'fab fa-google-plus', 'fab fa-linkedin', 'fab fa-instagram']
+        }),
+
+        computed: {
+            activeFab() {
+                switch (this.tabs) {
+                    case 'one': return { 'class': 'purple', icon: 'account_circle' }
+                    case 'two': return { 'class': 'red', icon: 'edit' }
+                    case 'three': return { 'class': 'green', icon: 'keyboard_arrow_up' }
+                    default: return {}
+                }
             }
-        }
+        },
+         watch: {
+      top (val) {
+        this.bottom = !val
+      },
+      right (val) {
+        this.left = !val
+      },
+      bottom (val) {
+        this.top = !val
+      },
+      left (val) {
+        this.right = !val
+      }
     }
+        }
+    
 </script>
 
