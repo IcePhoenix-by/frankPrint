24d9009fdaa3233144c4178a06ed1367129c76a4
Евгений <specterby@gmail.com>
2018-06-06 17:31:25 +0300

    qewqeqw

--- frankPrint/src/entries/page.js
+++ frankPrint/src/entries/page.js
@@ -1,6 +1,10 @@
 ﻿import Vue from 'vue';
 import Vuetify from 'vuetify'
 Vue.use(Vuetify)
+import $ from 'jquery'
+$('.nav-toggle').on('click', function () {
+    $('#menu').toggleClass('active');
+});
 import main from "../components/main.vue";
     new Vue({
         el: '#root',
